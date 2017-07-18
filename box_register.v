641286e23ac134ad9bcc0fb78933c1c616430d92
rarepep8 <alexw.wong@mail.utoronto.ca>
2017-07-18 16:57:18 -0400

    Completed box register, to be tested

--- box_register.v
+++ box_register.v
@@ -3,6 +3,8 @@ module box_register( // A register for holding and manipulating information abou
 	input  tap, // a pulse signal triggered by pos edge of an user input
 	output [6:0] y_coordinate 
 	);
+	reg curr_tap = 1'b0;
+    reg tapped = 1'b0;
     reg [3:0]fly_speed = 4'd0;
 	reg [6:0] current_y_coordinate = 7'd60;
 	assign y_coordinate[6:0] = current_y_coordinate[6:0];
@@ -10,9 +12,9 @@ module box_register( // A register for holding and manipulating information abou
     begin: box_location_update                
         if(current_y_coordinate <= 7'd120)
 			current_y_coordinate <= current_y_coordinate + 1; // During each game tick, the box falls 1 pixel
-        if(tap)
+        if(tapped)
 			fly_speed <= 4'd5;
-		  if(fly_speed > 0)
+		 if(fly_speed > 0)
             fly_speed <= fly_speed - 1'd1; // During each game tick, the upward speed of the box decreases by 1 pixel if it is not already 0
         if(fly_speed < current_y_coordinate)
             current_y_coordinate <= current_y_coordinate - fly_speed;
@@ -20,6 +22,14 @@ module box_register( // A register for holding and manipulating information abou
             current_y_coordinate <= 0;
 
     end // box_location_update
-    
 
+    always @(tap, game_clk)
+	 begin: fly_update
+        if(curr_tap != tap) begin
+            tapped <= 1'b1;
+				curr_tap <= tap;
+				end
+        else
+            tapped <= 1'b0;    
+    end //fly_update
 endmodule
